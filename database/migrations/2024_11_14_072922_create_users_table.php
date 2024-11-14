c12e57ac7d3d0e4f18ba1e9af3640ec484d9088f
yustinayasin <yustinayasin@gmail.com>
2024-11-14 15:27:37 +0700

    fix: migration order

--- database/migrations/2024_11_14_072922_create_users_table.php
+++ database/migrations/2024_11_14_072922_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('employees_id')->constrained()->onDelete('cascade');
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('status', ['active', 'inactive']);
